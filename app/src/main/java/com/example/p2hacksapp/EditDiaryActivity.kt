73de7aad9f17b9911729d66145a9a419cdc7214c
mika <b1021148@fun.ac.jp>
2022-12-15 23:26:01 +0900

    forth commit

---
+++ app/src/main/java/com/example/p2hacksapp/EditDiaryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.p2hacksapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditDiaryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_diary)
+    }
+}
